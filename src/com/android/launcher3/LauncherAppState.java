Descendant/packages_apps_Launcher3

Skip unneded package manager calls when checking QSB availability
Previous qsb toggle commit added a Search package availability check
that was being called also on some config changes (e.g. open an app,
rotate the screen, press home).
We can use the existing callbacks to avoid this.

src/com/android/launcher3/LauncherAppState.java
@@ -33,6 +33,8 @@ import com.android.launcher3.util.ConfigMonitor;
 import com.android.launcher3.util.Preconditions;
 import com.android.launcher3.util.SettingsObserver;
 
+import com.android.internal.util.du.Utils;
+
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 
@@ -55,6 +57,8 @@ public class LauncherAppState {
 
     private HomeKeyWatcher mHomeKeyListener = null;
     private boolean mNeedsRestart;
+
+    private boolean mIsSearchAppAvailable;
 
     public static LauncherAppState getInstance(final Context context) {
         if (INSTANCE == null) {
@@ -92,6 +96,8 @@ public class LauncherAppState {
         Log.v(Launcher.TAG, "LauncherAppState initiated");
         Preconditions.assertUIThread();
         mContext = context;
+
+        setSearchAppAvailable(Utils.isPackageInstalled(context, LauncherTab.SEARCH_PACKAGE));
 
         mInvariantDeviceProfile = new InvariantDeviceProfile(mContext);
         mIconCache = new IconCache(mContext, mInvariantDeviceProfile);
@@ -209,5 +215,13 @@ public class LauncherAppState {
                 .acquireContentProviderClient(LauncherProvider.AUTHORITY)) {
             return (LauncherProvider) cl.getLocalContentProvider();
         }
+    }
+
+    public void setSearchAppAvailable(boolean available) {
+        mIsSearchAppAvailable = available;
+    }
+
+    public boolean isSearchAppAvailable() {
+        return mIsSearchAppAvailable;
     }
 }

markakash/android_packages_apps_ParanoidLauncher

Change-Id: I532580eee0ee42a970b2f0550f322d6931267ba7

src/com/android/launcher3/LauncherAppState.java
@@ -39,6 +39,8 @@ import com.android.launcher3.util.Preconditions;
 import com.android.launcher3.util.SecureSettingsObserver;
 import com.android.launcher3.widget.custom.CustomWidgetManager;
 
+import com.android.internal.util.statix.Utils;
+
 public class LauncherAppState {
 
     public static final String ACTION_FORCE_ROLOAD = "force-reload-launcher";
@@ -56,6 +58,7 @@ public class LauncherAppState {
 
     private HomeKeyWatcher mHomeKeyListener = null;
     private boolean mNeedsRestart;
+    private boolean mIsSearchAppAvailable;
 
     public static LauncherAppState getInstance(final Context context) {
         return INSTANCE.get(context);
@@ -77,6 +80,8 @@ public class LauncherAppState {
         Log.v(Launcher.TAG, "LauncherAppState initiated");
         Preconditions.assertUIThread();
         mContext = context;
+
+        setSearchAppAvailable(Utils.isPackageInstalled(context, LauncherTab.SEARCH_PACKAGE));
 
         mInvariantDeviceProfile = InvariantDeviceProfile.INSTANCE.get(mContext);
         mIconCache = new IconCache(mContext, mInvariantDeviceProfile);
@@ -212,4 +217,13 @@ public class LauncherAppState {
             return (LauncherProvider) cl.getLocalContentProvider();
         }
     }
+
+    public void setSearchAppAvailable(boolean available) {
+        mIsSearchAppAvailable = available;
+    }
+
+    public boolean isSearchAppAvailable() {
+        return mIsSearchAppAvailable;
+    }
+
 }